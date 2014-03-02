2bd7c2e3a1ca09a4008812f856113d7187f5d62d
Jack Henley <jack.d.henley@gmail.com>
2014-03-02 15:16:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loopd::Application.config.secret_key_base = '1b125b2c02aadcac81f615bbab17413d7ba95a2b30177f2cfff521ad63e7803f9367285af4e1a62249fa9c37f93af845286eb495321a1705c975a93d55c01b28'
