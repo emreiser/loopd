0aba56d15c5d55995aaea83c76c981ea4b830f54
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-02 17:40:05 -0500

    Added Post model, migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140302220408) do
+ActiveRecord::Schema.define(version: 20140302223749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20140302220408) do
     t.text     "name"
     t.text     "url"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "title"
+    t.text     "url"
+    t.text     "author"
+    t.text     "content"
+    t.text     "pub_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
