199f5a1958cc7deed7b2413ac6ccf49623fe0376
Jack Henley <jack.d.henley@gmail.com>
2014-03-03 18:02:31 -0500

    changes redirect path

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,12 +1,10 @@
 class FeedsController < ApplicationController
   before_action :authenticate_user!
 
-  def actions
-  end
 
   def index
     @user = current_user
-    @feed = Feed.new
+    @feeds = @user.feeds
   end
 
   def new
@@ -18,7 +16,7 @@ class FeedsController < ApplicationController
 
     if @feed.save
       flash['notice'] = 'Feed added!'
-      redirect_to user_feeds_path
+      redirect_to feeds_path
     else
       flash.now['alert'] = @feed.errors.full_messages.join(', ')
       render :index
