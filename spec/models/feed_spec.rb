804e4f4a707467e0b082e1e0a2d6b7f27ebaf6f2
emreiser <emilie.reiser@gmail.com>
2014-03-03 10:45:29 -0500

    Wrote spec for category associations and passed. Deleted unnecessary spec files.

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Feed do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+  	it { should have_and_belong_to_many :categories }
+  	it { should belong_to :user }
+  end
 end
--- spec/models/view_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe View do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
