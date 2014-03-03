38b0f39392f17787fbbfbd2efdefa814408a552f
emreiser <emilie.reiser@gmail.com>
2014-03-03 12:32:53 -0500

    Completed feeds#create to add a new feed.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -11,8 +11,25 @@ class FeedsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @feed = current_user.feeds.new(feed_params)
+
+    if @feed.save
+      flash['notice'] = 'Feed added!'
+      redirect_to user_feeds_path
+    else
+      flash.now['alert'] = @feed.errors.full_messages.join(', ')
+      render :index
+    end
+
   end
 
   def show
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:name, :url)
   end
 end
