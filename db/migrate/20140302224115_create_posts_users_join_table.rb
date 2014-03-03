560f1bb886bf625b603f766d3128241e042d6b16
Jack Henley <jack.d.henley@gmail.com>
2014-03-03 11:15:40 -0500

    Added spec for category validation, edited join migrations

--- db/migrate/20140302224115_create_posts_users_join_table.rb
+++ db/migrate/20140302224115_create_posts_users_join_table.rb
@@ -1,8 +1,10 @@
 class CreatePostsUsersJoinTable < ActiveRecord::Migration
   def change
     create_join_table :posts, :users do |t|
-      # t.index [:post_id, :user_id]
-      # t.index [:user_id, :post_id]
+      t.index [:post_id, :user_id]
+      t.index [:user_id, :post_id]
+
+      t.timestamps
     end
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,11 @@ describe Category do
 		it { should have_and_belong_to_many :feeds }
 		it { should have_and_belong_to_many :posts }
 	end
+
+	describe 'validations' do
+
+		it 'is invalid without a name' do
+			expect(Category.create).to_not be_valid
+		end
+	end
 end
