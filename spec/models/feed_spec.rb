ce865a79f06bd0f43d452421eb19cfb37218a983
Jack Henley <jack.d.henley@gmail.com>
2014-03-03 22:03:08 -0500

    Fixed tests, all passing

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -3,24 +3,17 @@ require 'spec_helper'
 describe Feed do
   describe 'associations' do
   	it { should have_and_belong_to_many :categories }
-  	it { should belong_to :user }
+  	it { should have_and_belong_to_many :users }
   end
 
   describe 'validations' do
-  	it 'is invalid without a user id' do
-  		expect(Feed.create(name: 'NYT', url: 'http://www.nytimes.com')).to_not be_valid
-  	end
-
-  	it 'is invalid without a name' do
-  		expect(Feed.create(url: 'http://www.nytimes.com', user_id: 1)).to_not be_valid
-  	end
 
   	it 'is invalid without a url' do
-  		expect(Feed.create(name: 'NYT', user_id: 1)).to_not be_valid
+  		expect(Feed.create(name: 'NYT')).to_not be_valid
   	end
 
   	it 'is valid when all required fields are provided' do
-  		expect(Feed.create(name: 'NYT', url: 'http://www.nytimes.com', user_id: 1))
+  		expect(Feed.create(name: 'NYT', url: 'http://www.nytimes.com'))
   	end
   end
 end
