62967ef89647bc99cbe6ec4f5abcac496c5fc3fa
Jack Henley <jack.d.henley@gmail.com>
2014-03-04 14:18:35 -0500

    Passing user sign up test

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+feature 'Visitor signs up' do
+
+	scenario 'register as a user' do
+		visit root_path
+		click_on 'Sign up'
+		fill_in 'Email', with: 'jack@aol.com'
+		fill_in 'Password', with: 'password1'
+		fill_in 'Password confirmation', with: 'password1'
+		click_button 'Sign up'
+		expect(page).to have_content('jack@aol.com')
+	end
+
+end
