57a3f3496f38064e0b2de32d48df162ed281bc9d
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-04 16:49:07 -0500

    Passing test for unsuccessful submit (blank entry)

--- spec/features/user_adds_feed_spec.rb
+++ spec/features/user_adds_feed_spec.rb
@@ -8,10 +8,15 @@ feature 'User Signs In' do
 		sign_in_as(@user)
 	end
 
-	scenario 'Add a new feed' do
+	scenario 'Add a new feed successfully' do
 		fill_in 'Url', with: 'http://www.nytimes.com/services/xml/rss/nyt/Politics.xml'
 		click_on 'Add feed'
 		expect(page).to have_content('NYT > Politics')
 	end
 
+	scenario 'Tries to submit blank field' do
+		fill_in 'Url', with: ''
+		click_on 'Add feed'
+		expect(page).to have_content("URL can't be blank")
+	end
 end
