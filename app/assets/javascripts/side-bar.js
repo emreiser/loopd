48ae1593e748e71e932498a26e0ee588f55c72e1
emreiser <emilie.reiser@gmail.com>
2014-03-05 00:07:56 -0500

    Added AJAX and js to populate side-bar with feeds and categories. Tweaked side-bar css.

--- app/assets/javascripts/side-bar.js
+++ app/assets/javascripts/side-bar.js
@@ -1,10 +1,70 @@
 $(document).ready(function(){
 	$('#nav-bar-toggle-button').click(Loopd.toggleSideBar);
+	Loopd.getFeeds();
 
-})
+});
 
-var Loopd = {};
+var Loopd = Loopd || {};
 
 Loopd.toggleSideBar = function(){
 	$('#nav-bar-content').toggleClass('show');
-}
+};
+
+Loopd.getFeeds = function(){
+	$.ajax({
+		url: '/feeds',
+		type: 'GET',
+		dataType: 'json'
+	})
+
+	.done(function(data) {
+		console.log("success");
+		Loopd.feeds = JSON.parse(data.feeds);
+		Loopd.categories = data.categories;
+		Loopd.populateSideBar();
+	})
+
+	.fail(function() {
+		console.log("error");
+	})
+};
+
+Loopd.renderFeed = function(feed){
+	var new_feed = new Loopd.Feed(feed);
+	new_feed.addToAllFeeds();
+	new_feed.addToCategories();
+};
+
+Loopd.renderAllFeeds = function(feeds){
+	var i = 0, length = feeds.length;
+
+	for(;i < length;){
+		Loopd.renderFeed(feeds[i]);
+		i = i + 1;
+	};
+};
+
+Loopd.addCategory = function(category){
+	var cat_li, cat_ul;
+	cat_li = $('<li />', {html: category.name});
+	cat_ul = $('<ul />', {id: 'cat_' + category.id});
+	cat_li.append(cat_ul);
+
+	$('#all-categories').append(cat_li);
+};
+
+Loopd.addAllCategories = function(categories){
+	var i = 0, length = categories.length;
+
+	for(;i < length;){
+		Loopd.addCategory(categories[i]);
+		i = i + 1;
+	};
+};
+
+Loopd.populateSideBar = function(){
+	$('#all-feeds').empty()
+	$('#all-categories').empty()
+	Loopd.addAllCategories(Loopd.categories);
+	Loopd.renderAllFeeds(Loopd.feeds);
+};
