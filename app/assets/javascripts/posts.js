b8fa7537b4d78f5673c2ce61860b30e7e99b1a5e
Jack Henley <jack.d.henley@gmail.com>
2014-03-04 23:20:01 -0500

    Displaying external links to articles

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -13,6 +13,7 @@ Loopd.Post.prototype.buildPost = function() {
 	var postHTML = '<div class="post" id="post_feed_' + this.feed_id + '">';
 	postHTML += '<h2>' + this.title + '</h2>';
 	postHTML += '<div class="post_summary">' + this.summary + '</div>';
-	postHTML += '<div class="post_author">' + this.author + '</div>'
+	postHTML += '<div class="post_author">' + this.author + '</div>';
+	postHTML += '<a href="' + this.url + '" target="blank">Link to article</a>'
 	return postHTML;
 }
