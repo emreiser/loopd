36a7fcbe2c11c8ec7e471bc471c80675ad55d4ef
Jack Henley <jack.d.henley@gmail.com>
2014-03-05 12:05:57 -0500

    properly displaying and sorting post pub_dates

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,7 +2,7 @@ var Loopd = Loopd || {};
 
 Loopd.renderAllPosts = function(posts){
   $("#posts").empty();
-  posts.sort(function(a,b) { return (new Date(b.created_at) - new Date(a.created_at)) } );
+  posts.sort(function(a,b) { return (new Date(b.pub_date) - new Date(a.pub_date)) } );
 
   $.each(posts, function(i, post) {
     var new_post = new Loopd.Post(post);
