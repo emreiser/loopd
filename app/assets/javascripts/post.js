6652a49edc6600bfc5455ecd6d8757466244b572
Jack Henley <jack.d.henley@gmail.com>
2014-03-06 23:34:14 -0500

    Fixed error with null authors for posts

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -14,7 +14,12 @@ Loopd.Post = function(post) {
 Loopd.Post.prototype.buildPost = function() {
 	var postHTML = '<div class="post" id="post_feed_' + this.feed_id + '">';
 	postHTML += "<h3><a href='" + this.url + "' target='_blank'>" + this.title + "</a></h3>";
-	postHTML += '<div class="post-author">' + this.author + ' | Published: ' + Loopd.convertPostTime(this.pub_date) + '</div>';
+	if (this.author == null) {
+		postHTML += '<div class="post-author"> Published: ' + Loopd.convertPostTime(this.pub_date) + '</div>';
+	} else {
+		postHTML += '<div class="post-author">' + this.author + ' | ';
+		postHTML += ' Published: ' + Loopd.convertPostTime(this.pub_date) + '</div>';
+	}
 	postHTML += '<div class="post-summary"><p>' + this.summary + ' [...]</p></div>';
 	if(!($.isEmptyObject(this.content))) {
 		postHTML += '<div id="post_toggle_' + this.id + '">Click to toggle post</div>';
