549ed5c737dfc4b1bec1be8193ac688ed6a91818
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-03-07 10:49:17 -0500

    Added remaining tests for feed model

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -33,9 +33,55 @@ describe Feed do
   end
 
   describe '#update_feed' do
+    before :each do
+      @cabosanlupus = Feed.create(name: 'cabo san lupus', url: 'http://cabosanlupus.tumblr.com/rss')
+      response = @cabosanlupus.get_rss_response
+      @cabosanlupus.add_feed(response)
+      @cabosanlupus.update_feed(response)
+    end
+
+    it 'should populate feed with entries from rss response' do
+      expect(@cabosanlupus.add_posts(response.entries.first.title)).to be_instance_of('string')
+    end
   end
 
   describe '#add_posts' do
-  end
+    before :each do
+      @cabosanlupus = Feed.create(name: 'cabo san lupus', url: 'http://cabosanlupus.tumblr.com/rss')
+      response = @cabosanlupus.get_rss_response
+      @cabosanlupus.add_feed(response)
+      @cabosanlupus.update_feed(response)
+    end
 
+    it 'should create posts from returned data' do
+      expect(@cabosanlupus.add_posts(entries)).to contain(:title, :summary)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# none of these are passing due to changes in
+# models. the tests above are only passing because
+#'name' has been directly passed in, and the
+#'save' function works.
+
+      # the below fix was added by emilie directly
+      # to this spec file, but didn't work.
+      # fix should have been made
+      # to the .rb model file, but recent changes
+      # made it difficult to adjust the testing.
+
+      # response = @cabosanlupus.get_rss_response
+      # @cabosanlupus.add_feed(response)
+      # @cabosanlupus.update_feed(response)
